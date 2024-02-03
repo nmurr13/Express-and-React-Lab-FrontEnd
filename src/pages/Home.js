d02e27968f570d506e2a59e3ae9cc150fd49f0e0
Nicholas Murray <nmurr13@protonmail.com>
2024-02-03 10:38:09 -0800

    placeholder text for Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 function Home(props) {
-    return <h1>Home</h1>;
+    return <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Vitae et leo duis ut diam quam nulla porttitor. Diam in arcu cursus euismod quis viverra nibh cras pulvinar. Ac turpis egestas maecenas pharetra. Blandit libero volutpat sed cras ornare arcu dui vivamus arcu. Et ultrices neque ornare aenean. Bibendum enim facilisis gravida neque convallis. Egestas sed tempus urna et pharetra. Cras adipiscing enim eu turpis egestas pretium. Risus ultricies tristique nulla aliquet enim tortor. Diam quis enim lobortis scelerisque fermentum dui. 
+      Aliquam nulla facilisi cras fermentum. Odio eu feugiat pretium nibh.</p>;
   }
   
   export default Home;
